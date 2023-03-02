62eed02e4c0ab7b4148605942b41a36dcccb7caa
RanjoMondragon <rvmondragon96@gmail.com>
2023-03-02 05:23:44 -0500

    Remove hardcoded token

--- shop-frontend/src/requestMethods.js
+++ shop-frontend/src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/"
-//login in postman then get access token
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZWIzN2Q4NmQwMzg4ZDQxMzg3MWQwYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3Njc2NDQyNSwiZXhwIjoxNjc3MDIzNjI1fQ.6WKs_Xqbn7W_vWyO9kAWFH6W74Yr60uVnuXkcXegufg"
+const BASE_URL = "http://localhost:5000/api/";
+
+const token = localStorage.getItem('userToken');
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
@@ -10,5 +10,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    header : {token:`Bearer ${TOKEN}`}
+    headers: { Authorization: `Bearer ${token}` }
 });
